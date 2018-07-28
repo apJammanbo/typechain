96411ea25d69ed9a942be968c4c84507ef0a6542
apJammanbo <apJammanbo@gmail.com>
2018-07-28 16:58:53 +0900

    prettier 설정

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHello = (name, age, gender) => {
+    console.log(`${name} - ${age} - ${gender}`);
+};
+sayHello('min', 30, 'male');
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHello = (name:string, age:number, gender:string) => {
+    console.log(`${name} - ${age} - ${gender}`)
+}
+
+sayHello('min', 30, 'male');
+
+export {}
